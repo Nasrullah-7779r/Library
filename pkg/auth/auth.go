d4d3274159661ae3ae9eed81e93775c9572c7346
Muhammad Nasrullah <muhammad.nasrullah@aquila360.com>
2025-11-04 09:24:51 +0500

    Fix formatting in error message for token signing failure

--- pkg/auth/auth.go
+++ pkg/auth/auth.go
@@ -3,11 +3,12 @@ package auth
 import (
 	"errors"
 	"fmt"
-	"github.com/gin-gonic/gin"
 	"library/pkg/common"
 	"os"
 	"strings"
 	"time"
+
+	"github.com/gin-gonic/gin"
 
 	"github.com/golang-jwt/jwt/v5"
 )
@@ -34,7 +35,7 @@ func GenerateTokens(userCred common.LoginCred) Tokens {
 	t.AccessToken, err[0] = token.SignedString(secretKeyAccessToken)
 	t.RefreshToken, err[1] = refToken.SignedString(secretKeyRefreshToken)
 	if err[0] != nil && err[1] != nil {
-		fmt.Errorf("tokens didn't get signed %v", err)
+		fmt.Errorf("tokens didn't get signed    %v", err)
 		return Tokens{}
 	}
 	return t
